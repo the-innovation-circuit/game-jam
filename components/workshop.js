2ba9ba721f939505c0d8533001ac57f13141b018
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-05-13 19:52:11 +0800

    Update workshop.js

--- components/workshop.js
+++ components/workshop.js
@@ -1,13 +1,13 @@
 import { Box, Link, Text } from "theme-ui";
 import Icon from 'supercons'
 
-export default function liveWorkshop({ name, date }) {
+export default function liveWorkshop({ name, date, url = "https://www.youtube.com/channel/UCJb8wQnp0SHsN5vyhAZgklg" }) {
   return (
     <Box bg="sunken" p={2} px={3} sx={{ borderRadius: 5, mb: 2, display: 'flex', alignItems: 'center', gap: '6px' }}>
       <Box sx={{ flexGrow: 1 }}>
         <Text sx={{ fontWeight: 500 }} mr={1}>{name}</Text> ({date})
       </Box>
-      <Link href="https://www.youtube.com/channel/UCJb8wQnp0SHsN5vyhAZgklg" target="_blank" sx={{ display: 'flex', alignItems: 'center' }}>
+      <Link href={url} target="_blank" sx={{ display: 'flex', alignItems: 'center' }}>
         <Icon glyph="youtube" size="28" />
       </Link>
     </Box>
